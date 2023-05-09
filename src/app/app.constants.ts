1e88f2823173415e3b617c7c6893aa498f46f020
roberto-silva <roberto.silva.oli@outlook.com>
2023-05-09 19:37:38 -0300

    Updating api url

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,6 +1,6 @@
 export const ACCESS_TOKEN = "ACCESS_TOKEN";
-export const BASE_API = 'http://localhost:3000';
+export const BASE_API = 'http://34.228.17.139:3001';
 export const FIRST_VERSION_API = '/api/v1/';
-export const AUTH_API = BASE_API + FIRST_VERSION_API + 'auth';
+export const AUTH_API = BASE_API + FIRST_VERSION_API + 'seguranca/login';
 export const USER_API = BASE_API + FIRST_VERSION_API + 'users';
 export const PRODUCTS_API = BASE_API + FIRST_VERSION_API + 'products';
